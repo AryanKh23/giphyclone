6d0849a4aeaf9466c2ae847b1a8841fe3d3950be
Aryan Khotay <92273364+AryanKh23@users.noreply.github.com>
2023-11-27 02:59:58 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,27 @@
+// pages/login.js
+import { useState } from 'react';
+import { auth } from '../lib/firebase';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
